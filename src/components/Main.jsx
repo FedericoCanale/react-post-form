1f92a74d94088289ee71f072ba7f53b39de8678d
FedericoCanale <icocanale@gmail.com>
2025-11-13 12:54:36 +0100

    Add multifield state for post data

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Main() {
+    const [formData, setFormData] = useState({
+        author: "",
+        title: "",
+        body: "",
+        public: false,
+    });
+
     return (
         <main>
             <h1>Crea un nuovo post</h1>
@@ -8,22 +15,42 @@ export default function Main() {
             <form>
                 <div>
                     <label htmlFor="author">Author</label>
-                    <input id="author" name="author" type="text" />
+                    <input
+                        id="author"
+                        name="author"
+                        type="text"
+                        value={formData.author}
+                    />
                 </div>
 
                 <div>
                     <label htmlFor="title">Title</label>
-                    <input id="title" name="title" type="text" />
+                    <input
+                        id="title"
+                        name="title"
+                        type="text"
+                        value={formData.title}
+                    />
                 </div>
 
                 <div>
                     <label htmlFor="body">Body</label>
-                    <textarea id="body" name="body" rows="5" />
+                    <textarea
+                        id="body"
+                        name="body"
+                        rows="5"
+                        value={formData.body}
+                    />
                 </div>
 
                 <div>
                     <label htmlFor="public">
-                        <input id="public" name="public" type="checkbox" />
+                        <input
+                            id="public"
+                            name="public"
+                            type="checkbox"
+                            checked={formData.public}
+                        />
                         Public post
                     </label>
                 </div>
