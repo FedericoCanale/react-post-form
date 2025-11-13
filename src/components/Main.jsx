183c32a958ce2c58557062fe055d39a9173d8651
FedericoCanale <icocanale@gmail.com>
2025-11-13 12:40:23 +0100

    Add basic form structure (no logic)

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -4,6 +4,32 @@ export default function Main() {
     return (
         <main>
             <h1>Crea un nuovo post</h1>
+
+            <form>
+                <div>
+                    <label htmlFor="author">Author</label>
+                    <input id="author" name="author" type="text" />
+                </div>
+
+                <div>
+                    <label htmlFor="title">Title</label>
+                    <input id="title" name="title" type="text" />
+                </div>
+
+                <div>
+                    <label htmlFor="body">Body</label>
+                    <textarea id="body" name="body" rows="5" />
+                </div>
+
+                <div>
+                    <label htmlFor="public">
+                        <input id="public" name="public" type="checkbox" />
+                        Public post
+                    </label>
+                </div>
+
+                <button type="submit">Invia</button>
+            </form>
         </main>
     );
 }
