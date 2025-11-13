61c7ebd2fad0c9e918efa9779689bb40bfd181c8
FedericoCanale <icocanale@gmail.com>
2025-11-13 12:30:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Main from "./components/Main.jsx";
+
+export default function App() {
+  return (
+    <>
+      <Main />
+    </>
+  );
+}
+
+
---
+++ src/components/Main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Main() {
+    return (
+        <main>
+            <h1>Crea un nuovo post</h1>
+        </main>
+    );
+}
